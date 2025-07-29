e62b312c40c768eab6d395b22f36ba30481d2ace
cutiefunny <cutiefunny@gmail.com>
2025-07-30 01:43:39 +0900

    pwa 적용

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,37 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export const metadata = {
-  title: "Dope test app",
-  description: "마약검사앱",
+  applicationName: "dope-test",
+  title: {
+    default: "dope-test",
+    template: "dope-test",
+  },
+  description: "dope-test",
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "dope-test",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: "dope-test",
+    title: {
+      default: "dope-test",
+      template: "dope-test",
+    },
+    description: "dope-test",
+  }
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  themeColor: "lightgray",
 };
 
 export default function RootLayout({ children }) {

c193a7556b7970f7857bf44a893951f5a8ff0a4d
cutiefunny <cutiefunny@gmail.com>
2025-08-03 02:55:47 +0900

    selected 페이지 구성 중

--- app/test/[testType]/[kitId]/page.js
+++ app/test/[testType]/[kitId]/page.js
@@ -1,8 +1,8 @@
 'use client';
 
 import { useRouter, useParams } from 'next/navigation';
-import styles from '../../home/home.module.css';
-import commonStyles from '../../common.module.css';
+import styles from '../../../home/home.module.css';
+import commonStyles from '../../../common.module.css';
 import { useEffect, useState } from 'react';
 
 // 각 검사 유형별 키트 데이터
@@ -26,31 +26,23 @@ const kitData = {
 export default function TestKitSelectionPage() {
   const router = useRouter();
   const params = useParams();
+  const { kitId } = params;
   const { testType } = params;
+  const name = kitData[testType]?.find(kit => kit.id === parseInt(kitId))?.name || '선택된 키트';
 
   const [pageTitle, setPageTitle] = useState('');
-  const [kits, setKits] = useState([]);
-  const [selectedKit, setSelectedKit] = useState(null);
 
   useEffect(() => {
     if (testType === 'urine') {
       setPageTitle('소변으로 검사하기');
-      setKits(kitData.urine);
     } else if (testType === 'saliva') {
       setPageTitle('타액으로 검사하기');
-      setKits(kitData.saliva);
     } else {
       // 유효하지 않은 testType일 경우 홈으로 리디렉션
       router.push('/home');
     }
   }, [testType, router]);
 
-  const handleKitSelect = (kit) => {
-    setSelectedKit(kit);
-    router.push(`/test/${testType}/${kit.id}`);
-    console.log('Selected Kit:', kit);
-  };
-  
   // 뒤로가기 아이콘 컴포넌트
   const backIcon = () => (
     <img src="/images/back.png" alt="Back" style={{ width: '8px', height: '15px', marginLeft: '0.5rem' }} />
@@ -69,19 +61,15 @@ export default function TestKitSelectionPage() {
                 <span className={styles.pageTitle}>{pageTitle}</span>
                 <button onClick={() => router.back()} className={commonStyles.backButton}>{ArrowL()}</button>
             </div>
-                <div className={styles.kitGrid}>
-                {kits.map((kit) => (
-                    <div 
-                    key={kit.id} 
-                    className={`${styles.kitCard} ${selectedKit?.id === kit.id ? styles.selected : ''}`}
-                    onClick={() => handleKitSelect(kit)}
-                    >
-                    <div className={styles.kitImagePlaceholder}></div>
-                    <p className={styles.kitName}>{kit.name}</p>
-                    </div>
-                ))}
-                </div>
-            <p>사용 할 키트 선택하세요.</p>
+
+            <div className={styles.selectedKit}>
+              <p className={styles.kitName}>선택한 키트: {name}</p>
+            </div>
+
+            {/* 사진 촬영 버튼 */}
+            <button className={styles.captureButton} onClick={() => router.push(`/test/${testType}/${kitId}/capture`)}>
+              <span className={styles.captureButtonText}>사진 촬영</span>
+            </button>
         </div>
       </div>
     </div>

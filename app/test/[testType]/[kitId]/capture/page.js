34a24943925a533e3e9063f2863955244dcc8123
cutiefunny <cutiefunny@gmail.com>
2025-09-03 10:18:19 +0900

    re

--- app/test/[testType]/[kitId]/capture/page.js
+++ app/test/[testType]/[kitId]/capture/page.js
@@ -188,11 +188,12 @@ export default function CapturePage() {
       context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
       
       let imageDataUrl = "";
-      if (process.env.NODE_ENV === 'Production') {
-        imageDataUrl = canvas.toDataURL('image/png'); // 배포 시
-      } else {
-        imageDataUrl = "/images/dope-test1.jpg"; // 테스트 시
-      }
+      // if (process.env.NODE_ENV === 'Production') {
+      //   imageDataUrl = canvas.toDataURL('image/png'); // 배포 시
+      // } else {
+      //   imageDataUrl = "/images/dope-test1.jpg"; // 테스트 시
+      // }
+      imageDataUrl = canvas.toDataURL('image/png'); // 배포 시
       
       const resizedImageData = await resizeImage(imageDataUrl);
 
